fd8e017327cd6a745a7bdea7c091cc273a34b139
Jean-Jacques-Augustin <rjja@outlook.fr>
2023-05-06 21:31:14 +0300

    changement

---
+++ frontend/src/data/routes.js
@@ -0,0 +1,20 @@
+import GroupIcon from '@mui/icons-material/Group';
+
+
+export const routes = [
+    {
+        path: '/',
+        name : 'Users',
+        icon : <GroupIcon/>,
+    },
+    {
+        path: '/',
+        name : 'Candidates',
+        icon : <GroupIcon/>,
+    },
+    {
+        path: '/',
+        name : 'Vote',
+        icon : <GroupIcon/>,
+    }
+];
